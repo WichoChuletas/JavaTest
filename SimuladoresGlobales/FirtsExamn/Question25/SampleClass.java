dc8d9d75ba6969b8ed445855d075ac0a19900faa
WichoChuletas <fernando.arbovera@gmail.com>
2020-05-01 23:41:36 -0500

    Question25 about getClass()

---
+++ SimuladoresGlobales/FirtsExamn/Question25/SampleClass.java
@@ -0,0 +1,12 @@
+package SimuladoresGlobales.FirtsExamn.Question25;
+public class SampleClass {
+    public static void main(String[] args) {
+        AnotherSampleClass asc = new AnotherSampleClass();
+        SampleClass sc = new SampleClass();
+        sc = asc;
+        System.out.println("sc: " + sc.getClass());
+        System.out.println("asc: " + asc.getClass());
+    }
+}
+
+class AnotherSampleClass extends SampleClass {}
